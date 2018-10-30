32d501167316cf3e3a33c33db618f8a8268e3204
dkbrookie <matthew@dkbinnovative.net>
2018-10-30 14:16:05 -0500

    Update Function.OK-Popup.ps1

--- Function.OK-Popup.ps1
+++ Function.OK-Popup.ps1
@@ -4,20 +4,25 @@ Function OK-Popup {
     OK-Popup
 
     .DESCRIPTION
-    YesNo-Popup allows you to define a custom title and message for a popup to the end user by defining the `-Message` and `-Title` parameters.DESCRIPTION
+    YesNo-Popup allows you to define a custom title and message for a popup to the end user by defining the `-Message` and `-Title`
+    parameters. You can also chagne what the button says by defining the -Option1 parameter.
 
     .PARAMETER Message
-    Define the message you want to display to the end user here. Keep in mind if you want to use multiple lines, you have to start each new line with `n (not this is the key next to the number 1 key and not a single quote)
+    Define the message you want to display to the end user here. Keep in mind if you want to use multiple lines, you have to start
+    each new line with `n (not this is the key next to the number 1 key and not a single quote)
 
     .PARAMETER Title
     Define the title of the popup window here. If not specified, this will be "DKBInnovative"
 
     .PARAMETER Option1
-    Define what the button will say on the popup. Keep in mind that whatever your button says is exactly what the output will be if the user clicks that button. So if Option1 = "WOOHOO!" and the user clicks Option2, then the output of the popup to your console will be "WOOHOO!". The default value is "OK" unless defined in this parameter.DESCRIPTION
-    
+    Define what the button will say on the popup. Keep in mind that whatever your button says is exactly what the output will be if
+    the user clicks that button. So if Option1 = "WOOHOO!" and the user clicks Option2, then the output of the popup to your console
+    will be "WOOHOO!". The default value is "OK" unless defined in this parameter.DESCRIPTION
+
     .EXAMPLE
     C:\PS> YesNo-Popup -Message "This is my message to display on the popup" -Title "Custom title here"
     C:\PS> YesNo-Popup -Message "This is an example of how to use`nMultiple lines separated by using a backtick`nwhich is next to the 1 key"
+    C:\PS> YesNo-Popuyp -Message "This is the message that will be displayed to the user" -Option1 HI
   #>
 
   [CmdletBinding()]
