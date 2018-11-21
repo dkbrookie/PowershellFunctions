dc505a2235d4ccc07219657fb434aee4775540b8
dkbrookie <matthew@dkbinnovative.net>
2018-11-21 13:41:55 -0600

    Update Function.Get-FileDownload.ps1

--- Function.Get-FileDownload.ps1
+++ Function.Get-FileDownload.ps1
@@ -10,7 +10,6 @@ Function Get-FileDownload {
 
   Try {
     Start-BitsTransfer -Source $FileURL -Destination $Destination
-    }
   } Catch {
     Write-Error "There was an error while trying to download 7zip"
   }
