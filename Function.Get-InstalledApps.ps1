114dc0917781e35052fed5d9166c4f8fdf43e5ab
dkbrookie <matthew@dkbinnovative.net>
2018-11-29 10:36:57 -0600

    Create Function.Get-InstalledApps.ps1

---
+++ Function.Get-InstalledApps.ps1
@@ -0,0 +1,36 @@
+Function Get-InstalledApps {
+  <#
+    .SYNOPSIS
+    Get-InstalledApps
+
+    .DESCRIPTION
+    Finds the current install status of an application. If the application is NOT installed, the return will be NULL, if an
+    application IS installed, the return will be $True
+
+    .PARAMETER AppName
+    In quotes define the app name in question exactly as it's seen in Add Remove Programs in Control Panel
+
+    .EXAMPLE
+    C:\PS> Get-InstalledApps -AppName 'Microsoft SQL Server 2016'
+  #>
+
+  [CmdletBinding()]
+
+  Param(
+      [Parameter(Mandatory = $True)]
+      [string]$AppName
+  )
+
+  If([IntPtr]::Size -eq 4) {
+    $regPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+  } Else {
+    $regPath = @(
+      'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+      'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    )
+  }
+  $result = Get-ItemProperty $regPath | .{process{if($_.DisplayName -and $_.UninstallString) { $_ } }} | Select DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString | Where { $_.DisplayName -eq $AppName }
+  If($result) {
+    $True
+  }
+}
