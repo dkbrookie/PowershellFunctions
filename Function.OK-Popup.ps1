e90708ea4ff90c7655d81d93f7e0ad9674835dcb
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-03-01 22:05:09 -0600

    Update Function.OK-Popup.ps1

--- Function.OK-Popup.ps1
+++ Function.OK-Popup.ps1
@@ -1,5 +1,5 @@
 Function OK-Popup {
-  <#
+    <#
     .SYNOPSIS
     OK-Popup
 
@@ -23,79 +23,79 @@ Function OK-Popup {
     C:\PS> YesNo-Popup -Message "This is my message to display on the popup" -Title "Custom title here"
     C:\PS> YesNo-Popup -Message "This is an example of how to use`nMultiple lines separated by using a backtick`nwhich is next to the 1 key"
     C:\PS> YesNo-Popuyp -Message "This is the message that will be displayed to the user" -Option1 HI
-  #>
+    #>
 
-  [CmdletBinding()]
+    [CmdletBinding()]
 
-  Param(
-      [Parameter(Mandatory = $True)]
-      [string]$Message,
-      [string]$Title,
-      [string]$Option1
-  )
+    Param(
+        [Parameter(Mandatory = $True)]
+        [string]$Message,
+        [string]$Title,
+        [string]$Option1
+    )
 
-  $imgUrl = "https://support.dkbinnovative.com/labtech/transfer/assets/dkblogo.png"
-  $bgImage = "$env:windir\LTSvc\dkblogo.png"
-  If(!(Test-Path $bgImage -PathType Leaf)) {
-    Start-BitsTransfer -Source $imgUrl -Destination $bgImage
-  }
+    $imgUrl = "https://drive.google.com/uc?export=download&id=115V55-YXSvaPD6cuUQ_KzO0-SY8FSVW5"
+    $bgImage = "$env:windir\LTSvc\dkblogo.png"
+    If(!(Test-Path $bgImage -PathType Leaf)) {
+        (New-Object System.Net.WebClient).DownloadFile($imgUrl,$bgImage)
+    }
 
-  If(!$Message) {
-    $Message = "No message was specified"
-  }
+    If (!$Message) {
+        $Message = "No message was specified"
+    }
 
-  If(!$Title){
+    If (!$Title) {
     $Title = "DKBInnovative"
-  }
+    }
 
-  If(!$Option1) {
-    $Option1 = "OK"
-  }
+    If (!$Option1) {
+        $Option1 = "OK"
+    }
 
-  ##Load the Winforms assembly
-  [reflection.assembly]::LoadWithPartialName( "System.Windows.Forms") | Out-Null
-  Add-Type -AssemblyName System.Windows.Forms
+    # Load the Winforms assembly
+    [reflection.assembly]::LoadWithPartialName( "System.Windows.Forms") | Out-Null
+    Add-Type -AssemblyName System.Windows.Forms
 
-  $Icon = [system.drawing.icon]::ExtractAssociatedIcon("C:\Windows\LTSvc\labTech.ico")
-  $Background = [system.drawing.image]::FromFile($bgImage)
-  $Font = New-Object System.Drawing.Font("Roboto",10,[System.Drawing.FontStyle]::Regular)
-  $rebootForm = New-Object system.Windows.Forms.Form
-  $rebootForm.Text = $Title
-  $rebootForm.Icon = $Icon
-  $rebootForm.AutoScale = $True
-  $rebootForm.AutoSize = $True
-  $rebootForm.Font = $Font
-  $rebootForm.TopMost = $True
-  $rebootForm.AutoSizeMode = "GrowAndShrink"
-  $rebootForm.BackColor = "White"
-  $rebootForm.StartPosition = "CenterScreen"
-  $rebootForm.BackgroundImage = $Background
-  $rebootForm.BackgroundImageLayout = "Center"
+    $Icon = [system.drawing.icon]::ExtractAssociatedIcon("C:\Windows\LTSvc\labTech.ico")
+    $Background = [system.drawing.image]::FromFile($bgImage)
+    $Font = New-Object System.Drawing.Font("Roboto",11,[System.Drawing.FontStyle]::Regular)
+    $rebootForm = New-Object system.Windows.Forms.Form
+    $rebootForm.Text = $Title
+    $rebootForm.Icon = $Icon
+    $rebootForm.AutoScale = $True
+    $rebootForm.AutoSize = $True
+    $rebootForm.Font = $Font
+    $rebootForm.TopMost = $True
+    $rebootForm.AutoSizeMode = "GrowAndShrink"
+    $rebootForm.BackColor = "White"
+    $rebootForm.StartPosition = "CenterScreen"
+    $rebootForm.BackgroundImage = $Background
+    $rebootForm.BackgroundImageLayout = "Center"
 
-  $Label = New-Object System.Windows.Forms.Label
-  $Label.AutoSize = $True
-  $Label.BackColor = "Transparent"
-  $Label.textAlign = "MiddleCenter"
-  $Label.Dock = [System.Windows.Forms.DockStyle]::Top
-  $Label.Anchor = [System.Windows.Forms.AnchorStyles]::Left
-  $Label.Text = $message
+    $Label = New-Object System.Windows.Forms.Label
+    $Label.AutoSize = $True
+    $Label.BackColor = "Transparent"
+    $Label.textAlign = "MiddleCenter"
+    $Label.Dock = [System.Windows.Forms.DockStyle]::Top
+    $Label.Anchor = [System.Windows.Forms.AnchorStyles]::Left
+    $Label.Text = $message
 
-  ##Button 1
-  $button1 = new-object System.Windows.Forms.Button
-  $button1.Text = $Option1
-  $button1.AutoSize = $True
-  $button1.Dock = [System.Windows.Forms.DockStyle]::Bottom
-  $button1.Anchor = [System.Windows.Forms.AnchorStyles]::Right
-  $button1.Margin = 20
-  $button1.Location = New-Object Drawing.Point 35,40
-  $rebootForm.Controls.Add($button1)
-  ##Button 1 action
-  $button1.add_click({
-      $global:Answer = $Option1
-      $rebootForm.Close()
-  })
+    # Button 1
+    $button1 = new-object System.Windows.Forms.Button
+    $button1.Text = $Option1
+    $button1.AutoSize = $True
+    $button1.Dock = [System.Windows.Forms.DockStyle]::Bottom
+    $button1.Anchor = [System.Windows.Forms.AnchorStyles]::Right
+    $button1.Margin = 20
+    $button1.Location = New-Object Drawing.Point 35,40
+    $rebootForm.Controls.Add($button1)
+    # Button 1 action
+    $button1.add_click({
+    $global:Answer = $Option1
+    $rebootForm.Close()
+    })
 
-  $rebootForm.Controls.Add($Label)
-  $rebootForm.ShowDialog() | Out-Null
-  Write-Output "$Answer"
+    $rebootForm.Controls.Add($Label)
+    $rebootForm.ShowDialog() | Out-Null
+    Write-Output "$Answer"
 }
