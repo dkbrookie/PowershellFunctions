fb1760b6d6758376610988af6563132f757b6cab
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-06-04 13:57:16 -0500

    Added Install-EXE function
    -Removed default arguments in the MSI script except for /l to log to a specific dir. Since I'm letting the user define -Arguments and making it required now they will have to put something in and I don't want to confuse it with already having things like /qn defined for them.
    -For the EXE installer I removed the msiexec call and stgripped down the argument call on this one
    -Removed references of MSI arguments in the help docs for the EXE script

--- Function.Install-EXE.ps1
+++ Function.Install-EXE.ps1
@@ -1,27 +1,27 @@
-Function Install-MSI {
+Function Install-EXE {
   <#
     .SYNOPSIS
-    Install-MSI allows you to easily downooad and install an application via MSI
+    Install-EXE allows you to easily downooad and install an application via EXE
 
     .DESCRIPTION
-    This allows you to define as little as just the download link for the MSI installer and the rest
+    This allows you to define as little as just the download link for the EXE installer and the rest
     will be auto filled for you. If you decide to define more flags, like install dir or arguments,
     you will need to make sure these values are complete and in quotes if there are spaces. See examples
     for more informaiton.
 
     .PARAMETER AppName
     Define the name of your application. This will be used as the folder name for the downlaoded file, and
-    the name of the installer MSI oce downloaded.
+    the name of the installer EXE oce downloaded.
 
     .PARAMETER FileDownloadLink
-    This will be the download link to the MSI file. Make sure to include the FULL URL including the http://
+    This will be the download link to the EXE file. Make sure to include the FULL URL including the http://
 
     .PARAMETER FileDir
     This is the directory the download files and install logs will be saved to. If you leave this undefined,
     it will default to %windir%\LTSvc\packages\Software\AppName
 
-    .PARAMETER FileMSIPath
-    The full path to the MSI installer. If you had a specific location to the MSI file you would define it here.
+    .PARAMETER FileEXEPath
+    The full path to the EXE installer. If you had a specific location to the EXE file you would define it here.
     Keep in mind you do not have to define the -FileDownloadLink flag, so if you already had a local file or a
     network share file you can just define the path to it here.
 
@@ -30,12 +30,11 @@ Function Install-MSI {
     directory as your install files in %windir%\LTSvc\packages\Software\AppName\Install Log - App Name.txt
 
     .PARAMETER Arguments
-    Here you can define all arguments you want to use on the MSI. By defualt, /qn and /i will be applied for install
-    and silent, but if you define this parameter then you will need to add /i and /qn manually. See examples...
+    Here you can define all arguments you want to use on the EXE.
 
     .EXAMPLE
-    C:\PS> Install-MSI -AppName "SuperApp" -FileDownloadURL "https://domain.com/file/file.msi"
-    C:\PS> Install-MSI -AppName "SuperApp" -FileDownloadURL "https://domain.com/file/file.msi" -FileMSIPath "C:\windows\ltsvc\packages\softwar\superapp\superapp.msi" -LogPath "C:\install log.txt"
+    C:\PS> Install-EXE -AppName "SuperApp" -FileDownloadURL "https://domain.com/file/file.EXE -Arguments "/silent""
+    C:\PS> Install-EXE -AppName "SuperApp" -FileDownloadURL "https://domain.com/file/file.EXE" -FileEXEPath "C:\windows\ltsvc\packages\softwar\superapp\superapp.EXE" -Arguments "/s""
   #>
 
   [CmdletBinding()]
@@ -47,11 +46,10 @@ Function Install-MSI {
     )][string]$AppName,
     [string]$FileDownloadLink,
     [string]$FileDir,
-    [string]$FileMSIPath,
-    [string]$LogPath,
+    [string]$FileEXEPath,
     [Parameter(
       Mandatory=$True,
-      HelpMessage="Enter all arguments to install the MSI, such as /qn and /norestart"
+      HelpMessage="Enter all arguments to install the EXE, such as /s or /silent"
     )][string]$Arguments
   )
 
@@ -63,23 +61,23 @@ Function Install-MSI {
       New-Item -ItemType Directory $FileDir | Out-Null
     }
 
-    If (!$FileMSIPath) {
-      $FileMSIPath = "$FileDir\$($AppName).msi"
+    If (!$FileEXEPath) {
+      $FileEXEPath = "$FileDir\$($AppName).EXE"
     }
-    If(!(Test-Path $FileMSIPath -PathType Leaf)) {
-      (New-Object System.Net.WebClient).DownloadFile($FileDownloadLink,$FileMSIPath)
+    If(!(Test-Path $FileEXEPath -PathType Leaf)) {
+      (New-Object System.Net.WebClient).DownloadFile($FileDownloadLink,$FileEXEPath)
       }
 
     If (!$LogPath) {
       "$LogPath = $FileDir\Install Log - $($AppName).txt"
     }
   } Catch {
-    Write-Error "Failed to download $FileDownloadLink to $FileMSIPath"
+    Write-Error "Failed to download $FileDownloadLink to $FileEXEPath"
   }
   #endregion checkFiles
 
   Try {
-    Start-Process msiexec.exe -Wait -ArgumentList """$FileMSIPath"" $Arguments /l $LogPath"
+    Start-Process $FileEXEPath -Wait -ArgumentList $Arguments
     Write-Host "$AppName installation complete"
     #endregion installVAC
   } Catch {
