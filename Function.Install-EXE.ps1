5c757eb7379290585a33eba8043c32ae1bc180ca
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2021-02-01 00:03:48 -0600

    Update Function.Install-EXE.ps1
    -Greatly improved logging
    -Added installation verification steps
    -Updated documentation and script notes
    -Added additional steps to check for TLS1.2 and Powershell version since this has been found to be a fairly consistent issue with older systems (downloads failing w/ no TLS1.2 usually due to Powershell <2)

--- Function.Install-EXE.ps1
+++ Function.Install-EXE.ps1
@@ -10,8 +10,9 @@ Function Install-EXE {
     for more informaiton.
 
     .PARAMETER AppName
-    Define the name of your application. This will be used as the folder name for the downlaoded file, and
-    the name of the installer EXE oce downloaded.
+    IMPORTANT: Type the name EXACTLY as shown in Add/Remove programs in Control Panel. This will be used 
+    as the application name searched in Add/Remove programs after installation to verify this was successful,
+    used as the folder name for the setup file, and the name of the installer EXE once downloaded.
 
     .PARAMETER FileDownloadLink
     This will be the download link to the EXE file. Make sure to include the FULL URL including the http://
@@ -25,38 +26,81 @@ Function Install-EXE {
     Keep in mind you do not have to define the -FileDownloadLink flag, so if you already had a local file or a
     network share file you can just define the path to it here.
 
-    .PARAMETER LogPath
-    The full path to where you want the installation logs saved. By default, the logs will be saved in the same
-    directory as your install files in %windir%\LTSvc\packages\Software\AppName\Install Log - App Name.txt
-
     .PARAMETER Arguments
     Here you can define all arguments you want to use on the EXE.
 
+    .PARAMETER Wait
+    Valid values for this argument are $true or $false. This sets the -Wait flag on the Start-Process command of 
+    the installer EXE, meaning it will wait for the EXE to continue before moving on if this is set to $true. Set
+    to $true if undefined.
+
     .EXAMPLE
-    C:\PS> Install-EXE -AppName "SuperApp" -FileDownloadURL "https://domain.com/file/file.EXE -Arguments "/silent""
-    C:\PS> Install-EXE -AppName "SuperApp" -FileDownloadURL "https://domain.com/file/file.EXE" -FileEXEPath "C:\windows\ltsvc\packages\softwar\superapp\superapp.EXE" -Arguments "/s""
+    C:\PS> Install-EXE -AppName 'Microsoft Edge' -FileDownloadURL 'https://domain.com/file/file.EXE' -Arguments '/silent'
+    C:\PS> Install-EXE -AppName 'Microsoft Edge' -FileDownloadURL 'https://domain.com/file/file.EXE' -FileEXEPath 'C:\windows\ltsvc\packages\software\Microsoft Edge\Microsoft Edge.EXE' -Arguments '/s'
     #>
+
 
     [CmdletBinding()]
 
+
     Param(
-    [Parameter(
-        Mandatory=$True,
-        HelpMessage="Please enter the name of the application you want to install"
-    )][string]$AppName,
-    [string]$FileDownloadLink,
-    [string]$FileDir,
-    [string]$FileEXEPath,
-    [Parameter(
-        HelpMessage="Enter all arguments to install the EXE, such as /s or /silent"
-    )][string]$Arguments,
-    [Parameter(
-        HelpMessage='Sets the -Wait flag on the Start-Process command of the installer EXE, meaning it will wait for the EXE to continue before moving on if this is set to Y.'
-    )]
-    [ValidateSet('Y','N')]
-    [string]$Wait = 'Y'
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = "Please enter the name of the application you want to install exactly as seen in Add/Remove programs."
+        )]
+        [string]$AppName,
+        [string]$FileDownloadLink,
+        [string]$FileDir,
+        [string]$FileEXEPath,
+        [Parameter(
+            HelpMessage = "Enter all arguments to install the EXE, such as /s or /silent."
+        )]
+        [string]$Arguments,
+        [Parameter(
+            HelpMessage = 'Sets the -Wait flag on the Start-Process command of the installer EXE, meaning it will wait for the EXE to continue before moving on if this is set to $true. This is set to $true by default.'
+        )]
+        [boolean]$Wait = $true
     )
 
+    # To ensure successful downloads we need to set TLS protocal type to Tls1.2. Downloads regularly fail via Powershell without this step.
+    Try {
+        If (!([Net.SecurityProtocolType]::Tls12 )) {
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            $global:logOutput += "Successfully enabled TLS1.2 to ensure successful file downloads."
+        }
+    } Catch {
+        $global:logOutput += "Encountered an error while attempting to enable TLS1.2 to ensure successful file downloads. This can sometimes be due to dated Powershell. Checking Powershell version..."
+        # Generally enabling TLS1.2 fails due to dated Powershell so we're doing a check here to help troubleshoot failures later
+        $psVers = $PSVersionTable.PSVersion
+        If ($psVers.Major -lt 3) {
+            $global:logOutput += "Powershell version installed is only $psVers which has known issues with this script directly related to successful file downloads. Script will continue, but may be unsuccessful."
+        }
+    }
+    
+
+    # Quick function to check for successful application install after the installer runs
+    Function Get-InstalledApplications ($ApplicationName) {
+        # Applications may be in either of these locations depending on if x86 or x64
+        [array]$installedApps = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "*$ApplicationName*" }
+        [array]$installedApps += Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "*$ApplicationName*" }
+        If ((Get-PSDrive -PSProvider Registry).Name -notcontains 'HKU') {
+            New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+        }
+        # Applications can also install to single user profiles, so we're checking user profiles too
+        [array]$installedApps += Get-ItemProperty "HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "*$ApplicationName*" }
+        [array]$installedApps += Get-ItemProperty "HKU:\*\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "*$ApplicationName*" }
+        # Not using all of this output right now but nice to have it handy in case we want to output any of it later
+        $global:installedAppNames = $installedApps.DisplayName
+        $global:installedAppDate = $installedApps.InstallDate
+        $global:installedAppUninstallString = $installedApps.UninstallString
+        If ($installedApps.Count -gt 1) {
+            $global:logOutput += "Multiple applications found with the word(s) [$AppName] in the display name in Add/Remove programs. See list below..."
+            $global:logOutput += $installedAppNames
+        }
+    }
+
+
+    # Create all the dirs we need for a successful download/install
     Try {
         If (!$FileDir) {
             $FileDir = "$env:windir\LTSvc\packages\software\$AppName"
@@ -73,29 +117,39 @@ Function Install-EXE {
             (New-Object System.Net.WebClient).DownloadFile($FileDownloadLink,$FileEXEPath)
         }
     } Catch {
-        Write-Error "Failed to download $FileDownloadLink to $FileEXEPath"
+        [array]$global:logOutput += "Failed to download $FileDownloadLink to $FileEXEPath"
     }
-    #endregion checkFiles
 
+
+    # Since we added the option to NOT wait for the EXE to finish (on the off chance an EXE hangs after install, some do this for some reason) we need to
+    # set alternate start-process commands
     Try {
         If ($Arguments) {
-            If ($Wait = 'Y') {
+            If ($Wait) {
                 Start-Process $FileEXEPath -Wait -ArgumentList "$Arguments"
             } Else {
                 Start-Process $FileEXEPath -ArgumentList "$Arguments"
             }
         } Else {
-            If ($Wait = 'Y') {
+            If ($Wait) {
                 Start-Process $FileEXEPath -Wait
             } Else {
                 Start-Process $FileEXEPath
             }
         }
-        Write-Host "$AppName installation complete"
+        Get-InstalledApplications -ApplicationName $AppName
+        If ($global:installedAppNames) {
+            [array]$global:logOutput += "Verified the application name [$AppName] is now successfully showing in Add/Remove programs as installed! Script complete."
+        } Else {
+            [array]$global:logOutput += "$AppName is not reporting back as installed in Add/Remove Programs."
+        }
     } Catch {
-            Write-Error "Failed to install $AppName"
+        [array]$global:logOutput += "Failed to install $AppName. Full error output: $Error"
     }
 
     ## Delete the installer file
     Remove-Item $FileEXEPath -Force
+
+    $global:logOutput = $global:logOutput -join "`n"
+    $global:logOutput
 }
