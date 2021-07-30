95eae0b1ec74c2e25c145540f27b19b4a9d9135d
Seth Brasile <seth.brasile@gmail.com>
2021-07-29 20:12:48 -0500

    whoops

--- Function.Notify-ActiveUser.ps1
+++ Function.Notify-ActiveUser.ps1
@@ -284,7 +284,7 @@ function Notify-ActiveUser (
             [System.Windows.MessageBox]::Show($Message)
         }
 
-        $cmdCommand = "Powershell.exe -nologo -WindowStyle Hidden -Command `"Invoke-Command -ArgumentList $Message { $psCommand }`""
+        $cmdCommand = "Powershell.exe -nologo -WindowStyle Hidden -Command `"Invoke-Command -ArgumentList '$Message' { $psCommand }`""
         [DKB.ProcessExtensions.ProcessExtensions]::StartProcessAsCurrentUser($cmdCommand)
 }
 
