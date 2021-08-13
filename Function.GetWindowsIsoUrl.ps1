578b940398cb77df9fa486e548ccc5d02c3bf0a9
Seth Brasile <seth.brasile@gmail.com>
2021-08-13 13:18:05 -0500

    Create Function.GetWindowsIsoUrl.ps1

---
+++ Function.GetWindowsIsoUrl.ps1
@@ -0,0 +1,15 @@
+﻿function Get-WindowsIsoUrl {
+    param([string]$Rel = '20H2')
+
+    $fidoScript = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/Fido/master/Fido.ps1')
+
+    $params = @{
+        GetUrl = '$true'
+        Rel = $Rel
+    }
+
+    $fidoScriptBlock = [ScriptBlock]::create(".{$fidoScript} $(&{$args} @params)")
+
+
+    Return Invoke-Command -ScriptBlock $fidoScriptBlock
+}
