0948c5069327982fb247aa53ae1145f9250f163b
Seth Brasile <seth.brasile@gmail.com>
2022-01-11 08:58:04 -0600

    support win 11
    Added support for windows 11 in get-windowsversion and also added get-win11versioncomparison

--- Function.Get-WindowsVersion.ps1
+++ Function.Get-WindowsVersion.ps1
@@ -10,6 +10,7 @@
         Version = $Null
         Arch = $osArch
         OrderOfWin10Versions = @('1507','1511','1607','1703','1709','1803','1809','1903','1909','2004','20H2','21H1', '21H2')
+        OrderOfWin11Versions = @('21H2')
     }
 
     If ($osName -like 'Microsoft Windows Server 2008*' -and $osName -notlike '*R2*') {
@@ -56,6 +57,21 @@
             10586 { $osObject.Version = '1511'; }
             10240 { $osObject.Version = '1507'; }
             Default { $osObject.Name = 'Windows 10 Unknown'; $osObject.Version = 'Unknown'; }
+        }
+    } ElseIf ($osName -like 'Microsoft Windows 11*') {
+        # get raw Windows version
+        [int64]$rawVersion = [Windows.System.Profile.AnalyticsInfo,Windows.System.Profile,ContentType=WindowsRuntime].GetMember('get_VersionInfo').Invoke( $Null, $Null ).DeviceFamilyVersion
+
+        # decode bits to version bytes
+        $build = ( $rawVersion -band 0x00000000FFFF0000l ) -shr 16
+
+        $osObject.SimplifiedName = '11';
+        $osObject.Name = $osName;
+        $osObject.Build = "$build";
+
+        Switch ($build) {
+            22000 { $osObject.Version = '21H2'; }
+            Default { $osObject.Name = 'Windows 11 Unknown'; $osObject.Version = 'Unknown'; }
         }
     } ElseIf ($osName -like 'Microsoft Windows 8*') {
         $osObject.SimplifiedName = '8.1'
