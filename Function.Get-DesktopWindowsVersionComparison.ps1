845060b1e621c8031219ab88d982f9cbd38f0bd7
Seth Brasile <seth.brasile@gmail.com>
2022-02-14 09:01:56 -0600

    Add TLS fix

--- Function.Get-DesktopWindowsVersionComparison.ps1
+++ Function.Get-DesktopWindowsVersionComparison.ps1
@@ -21,6 +21,21 @@ If ($winIsLessThan20H2.Result) {
 }
 #>
 
+# Fix TLS
+Try {
+    # Oddly, this command works to enable TLS12 on even Powershellv2 when it shows as unavailable. This also still works for Win8+
+    [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+    $outputLog += "Successfully enabled TLS1.2 to ensure successful file downloads."
+} Catch {
+    $outputLog += "Encountered an error while attempting to enable TLS1.2 to ensure successful file downloads. This can sometimes be due to dated Powershell. Checking Powershell version..."
+    # Generally enabling TLS1.2 fails due to dated Powershell so we're doing a check here to help troubleshoot failures later
+    $psVers = $PSVersionTable.PSVersion
+
+    If ($psVers.Major -lt 3) {
+        $outputLog += "Powershell version installed is only $psVers which has known issues with this script directly related to successful file downloads. Script will continue, but may be unsuccessful."
+    }
+}
+
 # Call in Get-WindowsVersion
 (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/PowershellFunctions/master/Function.Get-WindowsVersion.ps1') | Invoke-Expression
 
