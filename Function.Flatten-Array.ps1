69c4eb09a33e443972eab6a96c9c7a048ccddc1a
Seth Brasile <seth.brasile@gmail.com>
2022-02-14 14:09:49 -0600

    New Flatten-Array function

---
+++ Function.Flatten-Array.ps1
@@ -0,0 +1,26 @@
+<#
+  .DESCRIPTION
+  Recursively flattens input array and removes all falsy values
+
+  .EXAMPLE
+  $deepArray = @('val1', @('val2', @('val3', 'val4'), '', 'val5'), $Null, 'val6')
+  Flatten-Array $deepArray
+
+  # Outputs -> @(val1, val2, val3, val4, val5, val6)
+#>
+
+function Flatten-Array {
+  Param(
+    [Parameter(Position = 0)]
+    [array]
+    $InputArray
+  )
+
+  Return $InputArray | ForEach-Object {
+    If ($_ -is [array]) {
+      Return Flatten-Array $_
+    } Else {
+      Return $_
+    }
+  } | Where-Object { $_ }
+}
