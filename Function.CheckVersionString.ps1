ae1556740d604b90ffc7ba5d61049444e444c1eb
Seth Brasile <seth.brasile@gmail.com>
2022-05-26 15:23:45 -0500

    more tests

--- Function.CheckVersionString.ps1
+++ Function.CheckVersionString.ps1
@@ -14,8 +14,6 @@ the specified version.
 
 If a star exists as one of the digits, any version will pass, from the point of the star onward.
 
-If both a
-
 .Parameter CheckAgainst
 Specifies the required/benchmark version string
 
@@ -80,7 +78,7 @@ Function Test-CheckVersionString {
 
   # These should all be $False
   If ($higherExact -ne $False) {
-    $failedTests += Format-Output '$higherExact'$False $higherExact $failedTests
+    $failedTests += Format-Output '$higherExact' $False $higherExact $failedTests
   }
 
   If ($lowerExact -ne $False) {
@@ -96,6 +94,104 @@ Function Test-CheckVersionString {
   }
 
   # ------------------------------------------------------------------------- #
+
+  # Carrot tests
+  $higherCarrot = Check-VersionString -Version '1.2.3' -CheckAgainst '^1.2.2'
+  $lowerCarrot = Check-VersionString -Version '1.2.3' -CheckAgainst '^1.2.4'
+  $lessSpecificCarrot = Check-VersionString -Version '1.2' -CheckAgainst '^1.2.4'
+  $moreSpecificCarrot = Check-VersionString -Version '1.2.3' -CheckAgainst '^1.2'
+
+  # Expecting $True
+  If ($higherCarrot -ne $True) {
+    $failedTests += Format-Output '$higherCarrot' $False $higherCarrot $failedTests
+  }
+
+  # Expecting $False
+  If ($lowerCarrot -ne $False) {
+    $failedTests += Format-Output '$lowerCarrot' $False $lowerCarrot $failedTests
+  }
+
+  # Expecting $False
+  If ($lessSpecificCarrot -ne $False) {
+    $failedTests += Format-Output '$lessSpecificCarrot' $False $lessSpecificCarrot $failedTests
+  }
+
+  # Expecting $True
+  If ($moreSpecificCarrot -ne $True) {
+    $failedTests += Format-Output '$moreSpecificCarrot' $False $moreSpecificCarrot $failedTests
+  }
+
+  # Star tests
+  $patchStarExact = Check-VersionString -Version '1.2.3' -CheckAgainst '1.2.*'
+  $patchStarHigher = Check-VersionString -Version '1.3.3' -CheckAgainst '1.2.*'
+  $patchStarLower = Check-VersionString -Version '1.1.3' -CheckAgainst '1.2.*'
+
+  $minorStarExact = Check-VersionString -Version '1.2.3' -CheckAgainst '1.*'
+  $minorStarHigher = Check-VersionString -Version '1.3.3' -CheckAgainst '1.*'
+  $minorStarLower = Check-VersionString -Version '1.1.3' -CheckAgainst '1.*'
+  $minorStarMajorHigher = Check-VersionString -Version '2.1.3' -CheckAgainst '1.*'
+  $minorStarMajorLower = Check-VersionString -Version '0.1.3' -CheckAgainst '1.*'
+
+  $majorStarOne = Check-VersionString -Version '1' -CheckAgainst '*'
+  $majorStarTwo = Check-VersionString -Version '1.2' -CheckAgainst '*'
+  $majorStarThree = Check-VersionString -Version '1.2.3' -CheckAgainst '*'
+
+  If ($patchStarExact -ne $True) {
+    $failedTests += Format-Output '$patchStarExact' $False $patchStarExact $failedTests
+  }
+  If ($patchStarHigher -ne $False) {
+    $failedTests += Format-Output '$patchStarHigher' $False $patchStarHigher $failedTests
+  }
+  If ($patchStarLower -ne $False) {
+    $failedTests += Format-Output '$patchStarLower' $False $patchStarLower $failedTests
+  }
+
+  If ($minorStarExact -ne $True) {
+    $failedTests += Format-Output '$minorStarExact' $True $minorStarExact $failedTests
+  }
+  If ($minorStarHigher -ne $True) {
+    $failedTests += Format-Output '$minorStarHigher' $True $minorStarHigher $failedTests
+  }
+  If ($minorStarLower -ne $True) {
+    $failedTests += Format-Output '$minorStarLower' $True $minorStarLower $failedTests
+  }
+  If ($minorStarMajorHigher -ne $False) {
+    $failedTests += Format-Output '$minorStarMajorHigher' $False $minorStarMajorHigher $failedTests
+  }
+  If ($minorStarMajorLower -ne $False) {
+    $failedTests += Format-Output '$minorStarMajorLower' $False $minorStarMajorLower $failedTests
+  }
+
+  If ($majorStarOne -ne $True) {
+    $failedTests += Format-Output '$majorStarOne' $True $majorStarOne $failedTests
+  }
+  If ($majorStarTwo -ne $True) {
+    $failedTests += Format-Output '$majorStarTwo' $True $majorStarTwo $failedTests
+  }
+  If ($majorStarThree -ne $True) {
+    $failedTests += Format-Output '$majorStarThree' $True $majorStarThree $failedTests
+  }
+
+
+  # Expecting $True
+  If ($higherCarrot -ne $True) {
+    $failedTests += Format-Output '$higherCarrot' $False $higherCarrot $failedTests
+  }
+
+  # Expecting $False
+  If ($lowerCarrot -ne $False) {
+    $failedTests += Format-Output '$lowerCarrot' $False $lowerCarrot $failedTests
+  }
+
+  # Expecting $False
+  If ($lessSpecificCarrot -ne $False) {
+    $failedTests += Format-Output '$lessSpecificCarrot' $False $lessSpecificCarrot $failedTests
+  }
+
+  # Expecting $True
+  If ($moreSpecificCarrot -ne $True) {
+    $failedTests += Format-Output '$moreSpecificCarrot' $False $moreSpecificCarrot $failedTests
+  }
 
   # Enumerate results
   $numFailed = $failedTests.length
