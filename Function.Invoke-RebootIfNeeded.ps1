6b2a7c63c86cc77c9c0bdc6bbc55491a3f8ec8fc
Seth Brasile <seth.brasile@gmail.com>
2023-03-30 12:30:53 -0500

    normalize TLS error output on new functions

--- Function.Invoke-RebootIfNeeded.ps1
+++ Function.Invoke-RebootIfNeeded.ps1
@@ -2,12 +2,12 @@ Try {
   # Oddly, this command works to enable TLS12 on even Powershellv2 when it shows as unavailable. This also still works for Win8+
   [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
 } Catch {
-  $output += "Encountered an error while attempting to enable TLS1.2 to ensure successful file downloads. This can sometimes be due to dated Powershell. Checking Powershell version..."
+  Write-Output "Encountered an error while attempting to enable TLS1.2 to ensure successful file downloads. This can sometimes be due to dated Powershell. Checking Powershell version..."
   # Generally enabling TLS1.2 fails due to dated Powershell so we're doing a check here to help troubleshoot failures later
   $psVers = $PSVersionTable.PSVersion
 
   If ($psVers.Major -lt 3) {
-    $output += "Powershell version installed is only $psVers which has known issues with this script directly related to successful file downloads. Script will continue, but may be unsuccessful."
+    Write-Output "Powershell version installed is only $psVers which has known issues with this script directly related to successful file downloads. Script will continue, but may be unsuccessful."
   }
 }
 
