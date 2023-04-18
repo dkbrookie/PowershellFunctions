ce2ac217e0c935ad2f8712d2bd8dc4d62bbe9bcc
Seth Brasile <seth.brasile@gmail.com>
2023-04-18 15:37:55 -0500

    Create Function.UpgradeWin10.ps1

---
+++ Function.UpgradeWin10.ps1
@@ -0,0 +1,14 @@
+$dir = "c:\temp"
+mkdir $dir -ErrorAction SilentlyContinue
+[Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+$webClient = New-Object System.Net.WebClient
+$url = "https://go.microsoft.com/fwlink/?LinkID=799445"
+$file = "$($dir)\Win10Upgrade.exe"
+$webClient.DownloadFile($url,$file)
+$proc = Start-Process -FilePath $file -PassThru -Wait -ArgumentList "/quietinstall /skipeula /auto upgrade /copylogs /NoReboot /NoRestartUI /NoRestart $dir"
+
+if ($proc.ExitCode -eq 0) {
+  Write-Host "!Success: Upgrade successful"
+} else {
+  Write-Host "!Error: Upgrade failed. StandardError Output: " + $proc.StandardError
+}
